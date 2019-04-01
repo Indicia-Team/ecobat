4a265622d9f48178ba814717acd0eba4fafc154a
John van Breda <john.vanbreda@biodiverseit.co.uk>
2019-04-01 15:58:56 +0100

    Add fields for method of classification and software used

---
+++ warehouse_modules/ecobat/db/version_2_0_0/201903041100_additional_fields.sql
@@ -0,0 +1,14 @@
+ALTER TABLE ecobat_occurrences
+  ADD COLUMN method_of_classification character_varying,
+  ADD COLUMN analysis_software_used character_varying;
+
+COMMENT ON COLUMN ecobat_occurrences.method_of_classification IS 'Method of identification of the bat species.';
+COMMENT ON COLUMN ecobat_occurrences.analysis_software_used IS 'Analysis software used for the data.';
+
+ALTER TABLE ecobat_occurrence_passes
+  ADD COLUMN method_of_classification character_varying,
+  ADD COLUMN analysis_software_used character_varying;
+
+COMMENT ON COLUMN ecobat_occurrence_passes.method_of_classification IS 'Method of identification of the bat species.';
+COMMENT ON COLUMN ecobat_occurrence_passes.analysis_software_used IS 'Analysis software used for the data.';
+
