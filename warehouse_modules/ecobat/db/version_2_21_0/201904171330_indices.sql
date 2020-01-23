1290707df98f34bb3e40fec59ea080e1a15651e2
John van Breda <john.vanbreda@biodiverseit.co.uk>
2020-01-23 11:28:16 +0000

    Adds indexes for performance

--- warehouse_modules/ecobat/db/version_2_21_0/201904171330_indices.sql
+++ warehouse_modules/ecobat/db/version_2_21_0/201904171330_indices.sql
@@ -6,3 +6,10 @@ CREATE INDEX ix_ecobat_occurrence_ref_set ON ecobat_occurrences(pass_definition_
 
 DROP INDEX IF EXISTS ix_ecobat_occurrence_ref_set_locality;
 CREATE INDEX ix_ecobat_occurrence_ref_set_locality ON ecobat_occurrences(easting, northing);
+
+DROP INDEX IF EXISTS ix_erccis_summary_import_lookup;
+CREATE index ix_erccis_summary_import_lookup ON cache_taxa_taxon_lists(external_key)
+WHERE taxon_list_id=1 AND preferred=true;
+
+DROP INDEX IF EXISTS ix_erccis_summary_occurrences_unique_id;
+CREATE index ix_erccis_summary_occurrences_unique_id ON erccis_summary_occurrences(unique_id);
